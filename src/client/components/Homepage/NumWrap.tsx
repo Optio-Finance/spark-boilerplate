a930769fb144a927eafcd2442d6799395665534f
azat-hafizov <azat@optio.finance>
2022-09-27 22:59:32 +0500

    fix: update inconsistencies and typos

--- src/client/components/Homepage/NumWrap.tsx
+++ src/client/components/Homepage/NumWrap.tsx
@@ -4,7 +4,7 @@ export const NumWrap = (digit: number) => {
   return (
     <span
       className={classNames(
-        "px-1.5 py-0.5 rounded lining-nums slashed-zero ",
+        "px-1.5 py-0.5 rounded lining-nums slashed-zero",
         "bg-black text-white"
       )}
     >
