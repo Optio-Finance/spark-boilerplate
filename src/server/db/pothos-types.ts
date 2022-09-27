9c73570d40b2118ee3821ec6e89332390c699040
azat-hafizov <azat@optio.finance>
2022-09-27 23:13:16 +0500

    feat: update Pothos integration with Prisma

--- src/server/db/pothos-types.ts
+++ src/server/db/pothos-types.ts
@@ -1,12 +1,14 @@
-import type { Prisma, User, Project, Account, Session, VerificationToken } from "/Users/azat/optio/spark/node_modules/.prisma/client";
+/* eslint-disable */
+import type { Prisma, User, Project, Account, Session, VerificationToken } from "@prisma/client";
 export default interface PrismaTypes {
     User: {
         Name: "User";
         Shape: User;
         Include: Prisma.UserInclude;
         Select: Prisma.UserSelect;
-        Where: Prisma.UserWhereUniqueInput;
-        Fields: "projects" | "accounts" | "sessions";
+        OrderBy: Prisma.UserOrderByWithRelationInput;
+        WhereUnique: Prisma.UserWhereUniqueInput;
+        Where: Prisma.UserWhereInput;
         RelationName: "projects" | "accounts" | "sessions";
         ListRelations: "projects" | "accounts" | "sessions";
         Relations: {
@@ -29,8 +31,9 @@ export default interface PrismaTypes {
         Shape: Project;
         Include: Prisma.ProjectInclude;
         Select: Prisma.ProjectSelect;
-        Where: Prisma.ProjectWhereUniqueInput;
-        Fields: "users";
+        OrderBy: Prisma.ProjectOrderByWithRelationInput;
+        WhereUnique: Prisma.ProjectWhereUniqueInput;
+        Where: Prisma.ProjectWhereInput;
         RelationName: "users";
         ListRelations: "users";
         Relations: {
@@ -45,8 +48,9 @@ export default interface PrismaTypes {
         Shape: Account;
         Include: Prisma.AccountInclude;
         Select: Prisma.AccountSelect;
-        Where: Prisma.AccountWhereUniqueInput;
-        Fields: "user";
+        OrderBy: Prisma.AccountOrderByWithRelationInput;
+        WhereUnique: Prisma.AccountWhereUniqueInput;
+        Where: Prisma.AccountWhereInput;
         RelationName: "user";
         ListRelations: never;
         Relations: {
@@ -61,8 +65,9 @@ export default interface PrismaTypes {
         Shape: Session;
         Include: Prisma.SessionInclude;
         Select: Prisma.SessionSelect;
-        Where: Prisma.SessionWhereUniqueInput;
-        Fields: "user";
+        OrderBy: Prisma.SessionOrderByWithRelationInput;
+        WhereUnique: Prisma.SessionWhereUniqueInput;
+        Where: Prisma.SessionWhereInput;
         RelationName: "user";
         ListRelations: never;
         Relations: {
@@ -77,8 +82,9 @@ export default interface PrismaTypes {
         Shape: VerificationToken;
         Include: never;
         Select: Prisma.VerificationTokenSelect;
-        Where: Prisma.VerificationTokenWhereUniqueInput;
-        Fields: never;
+        OrderBy: Prisma.VerificationTokenOrderByWithRelationInput;
+        WhereUnique: Prisma.VerificationTokenWhereUniqueInput;
+        Where: Prisma.VerificationTokenWhereInput;
         RelationName: never;
         ListRelations: never;
         Relations: {};
